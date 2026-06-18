ef655a247f29865e35a0d52256c9a85ae59c4329
Alina <alina_gabur@yahoo.com>
2026-06-18 12:21:14 +0200

    lists en tuples types

--- Learn Python-Full Course for Beginners/app.py
+++ Learn Python-Full Course for Beginners/app.py
@@ -25,8 +25,55 @@ print(sqrt(36))
 # age = input("Enter your age: ")
 # print("Hello " + name + "! You are " + age + " years old ")
 
-num1 = input("Enter a number: ")
-num2= input("Enter another number: ")
-# result = int(num1)+int(num2)
-result = float(num1) +float(num2)
-print(result)
+# num1 = input("Enter a number: ")
+# num2= input("Enter another number: ")
+# # result = int(num1)+int(num2)
+# result = float(num1) +float(num2)
+# print(result)
+
+# Mad Libs Game
+# color = input("Enter a color: ")
+# plural_noun = input("Answer a Plural Noun: ")
+# celebrity = input("Enter a celebrity: ")
+
+
+# print("Roses are " + color)
+# print( plural_noun + " are blue")
+# print("I love " + celebrity)
+
+# Lists
+lucky_numbers=[2, 7, 12, 3, 54, 105, 100]
+friends = ["Mike", "Maria", "Mike", "John", "Anna", "Mira"]
+friends[1]= "Julia"
+print(friends[0])
+print(friends[3])
+print(friends[-1])
+print(friends[1:])
+print(friends[-2:4])
+
+friends.sort()
+print(friends)
+lucky_numbers.sort()
+# lucky_numbers.reverse()
+print(lucky_numbers)
+
+friends.extend(lucky_numbers)
+friends.append("Kevin")
+friends.insert(1, "Eline")
+# friends.remove('Mira')
+# friends.clear() # removes all the elements in de list
+# friends.pop() # removes the last element in de list
+print(friends.index("Kevin"))
+print(friends.count("Mike"))
+friends2 = friends.copy()
+print(friends2)
+
+# Tuples
+coordinates = ((4, 5), (6,7), (80,45))
+print(coordinates)
+# coordinates[1] = 10 # krijg error
+print(coordinates[0])
+
+
+
+
