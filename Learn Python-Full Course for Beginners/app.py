da8dc6ef7f329acaa216cc94d37c1615cba0c69e
Alina <alina_gabur@yahoo.com>
2026-06-19 15:27:28 +0200

    exponent function, lists & nested Loopsn

--- Learn Python-Full Course for Beginners/app.py
+++ Learn Python-Full Course for Beginners/app.py
@@ -200,3 +200,51 @@ for index in range(5):
         print("First Iteration")
     else:
         print("Not first")
+
+
+# Exponent Function
+
+print(2 ** 3) 
+
+def raise_to_power(base_num, pow_num):
+   result = 1
+   for index in range(pow_num):
+       result = result * base_num
+   return result
+
+
+print(raise_to_power(3,3))
+
+
+# 2D Lists & Nested Loops
+
+number_grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+
+print(number_grid[0][0])
+
+for row in number_grid:
+    for col in row:
+        print(col)
+
+# Build a Translator
+   
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            if letter.isupper():
+
+               translation = translation + "G"
+            else:
+                translation= translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
+
