ccde692fe920901132159ccd9dff9e779f5b7478
Alina <alina_gabur@yahoo.com>
2026-06-24 15:28:20 +0200

    string methods

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -325,8 +325,40 @@ user_role = "admin"
 # status = "Adult" if age >= 18 else "Child"
 # print(status)
 
-weather = "HOT" if temperature > 20 else "COLD"
-access_level = "Full Access" if user_role== "admin" else "Limited Access"
-print(access_level)
-print(weather)
+# weather = "HOT" if temperature > 20 else "COLD"
+# access_level = "Full Access" if user_role== "admin" else "Limited Access"
+# print(access_level)
+# print(weather)
 
+
+# String methodes
+# name = input("Enter your full name: ")
+phone_number = input("Enter your phone number")
+# result = len(name)
+# result = name.find(" ")
+# name = name.capitalize()
+# name = name.upper()
+# name - name.lower()
+# result = name.isdigit()
+# result= name.isalpha()
+# result = phone_number.count("-")
+# result = phone_number.replace("-", " ")
+print(result)
+# print(help(str))
+
+
+# validate user input exercise
+# 1. username is no more than 12 characters
+# 2. username must not contain spaces
+# 3. username must not contain digits
+
+username = input("Enter a username: ")
+
+if len(username) >12:
+    print("Your username can't be more than 12 characters")
+elif not username.find(" ") == -1:
+    print("Your username can't contain spaces")
+elif not username.isalpha():
+    print("Your username can't contain numbers")
+else:
+    print(f"Welcome {username}")
