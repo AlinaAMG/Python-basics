8c529220287fb6ea8420d5eed1ec9f61b9e4c826
Alina <alina_gabur@yahoo.com>
2026-06-24 12:27:03 +0200

    conditional expression

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -297,3 +297,36 @@ elif temp <= 0 and not is_sunny:
 elif 28>temp > 0 and not is_sunny:
     print("It is WARM outside")
     print("It is CLOUDY ☁️")
+
+
+# Conditional expression = A one-line shortcut for the if-else statement (ternary operator)
+#  Print or assign one of the values based on a condition
+#  X if condition else Y
+
+
+num = -3
+a = 6
+b = 7
+age = 25
+temperature = 10
+user_role = "admin"
+
+
+# print("Positive" if num > 0 else "Negative")
+# result = "EVEN" if num % 2 == 0 else "ODD"
+# print(result)
+
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+
+# print(max_num)
+# print(min_num)
+
+# status = "Adult" if age >= 18 else "Child"
+# print(status)
+
+weather = "HOT" if temperature > 20 else "COLD"
+access_level = "Full Access" if user_role== "admin" else "Limited Access"
+print(access_level)
+print(weather)
+
