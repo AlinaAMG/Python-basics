cc726e10edb7525a76fe0067f3a47fd5a6b91595
Alina <alina_gabur@yahoo.com>
2026-06-25 11:03:33 +0200

    while loops

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -407,3 +407,35 @@ price3 = 12.34
 print(f"Price 1 is {price1:10}")
 print(f"Price 2 is {price2:10}")
 print(f"Price 3 is {price3:10}")
+
+# WHILE LOOP = execute some code WHILE some condition remains true
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# while name == "":
+#     print("You did  not enter your name")
+#     name = input("Enter your name: ")
+# print(f"Hello {name}")
+
+while age < 0:
+    print("Age can't be negative")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old")
+
+food = input("Enter a food you like (q to quite): ")
+
+while not food == "q":
+    print(f"You like {food}")
+    food = input("Enter another food you like (q to quite): ")
+    
+print("bye")
+
+num = input("Enter a number between 1-10: ")
+
+while num < 1 or num > 10 :
+    print(f"{num} is not valid")
+    num = int(input("Enter a number between 1-10: "))
+    
+print(f"Your number is {num}")
