e668a0bf29881cc53617ff25723536530f9013da
Alina <alina_gabur@yahoo.com>
2026-06-29 12:14:41 +0200

    shopping cart program

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -565,3 +565,55 @@ print(fruits)
 fruits = ("apple", "orange", "banana", "coconut", "coconut")
 # print(fruits.index("apple"))
 print(fruits.count("coconut"))
+
+# Shopping cart program
+
+# foods = []
+# prices = []
+# total = 0
+
+# while True:
+#     food = input("Enter a food to buy (q to quit): ")
+#     if food.lower() == "q": 
+#         break
+#     else: 
+#         price = input(f"Enter the price of a {food}: $")
+#         foods.append(food)
+#         prices.append(price)
+
+# print("-------YOUR CART-------")
+
+# for food in foods:
+#     print(food, end =" ")
+
+# for price in prices :
+#     total += price
+
+# print(f"Your total is: ${total}")
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+# vegetables = ["celery", "carrots", "potatoes"]
+# meats =["chicken","fish", "turkey"]
+
+# groceries = [fruits, vegetables, meats]
+groceries = [['apple', 'orange', 'banana', 'coconut'], ['celery', 'carrots', 'potatoes'], ['chicken', 'fish', 'turkey']]
+
+for collection in groceries:
+  for food in collection:
+    print(food, end=" ")
+  print()
+
+# print(groceries)
+
+num_pad = ((1, 2, 3),
+          (4,5,6), 
+          (7,8,9), 
+          ("*" , 0, "#"))
+
+for row in num_pad:
+    for num in row:
+        print(num, end = " ")
+    print()
+
+# Python quiz game
+
