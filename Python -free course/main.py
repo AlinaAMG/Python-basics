e3be29f148daaf5c557546d94463a51202cb21c1
Alina <alina_gabur@yahoo.com>
2026-06-30 14:30:14 +0200

    game rock, paper, scissors

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -761,38 +761,70 @@ print(cards)
 
 # Python number guessing game
 
-lowest_num = 1
-highest_num = 100
-answer = random.randint(lowest_num, highest_num)
-guesses = 0
-is_running = True
+# lowest_num = 1
+# highest_num = 100
+# answer = random.randint(lowest_num, highest_num)
+# guesses = 0
+# is_running = True
 
-print("Python Number Guessing Game")
-print(f"Select a number between {lowest_num} and {highest_num}")
+# print("Python Number Guessing Game")
+# print(f"Select a number between {lowest_num} and {highest_num}")
 
-while is_running:
+# while is_running:
 
-    guess = input("Enter your guess: ")
+#     guess = input("Enter your guess: ")
 
-    if guess.isdigit():
-        guess = int(guess)
-        guesses += 1
+#     if guess.isdigit():
+#         guess = int(guess)
+#         guesses += 1
 
-        if guess < lowest_num or guess > highest_num:
-            print("That number is out of range")
-            print(f"Please select a number between {lowest_num} and {highest_num}")
-        elif guess < answer:
-            print("Too low! Try again!")
-        elif guess > answer:
-            print("Too high!Try again")
-        else:
-            print(f"CORRECT! The answer was {answer}")
-            print(f"Number of guesses: {guesses}")
-            is_running = False
+#         if guess < lowest_num or guess > highest_num:
+#             print("That number is out of range")
+#             print(f"Please select a number between {lowest_num} and {highest_num}")
+#         elif guess < answer:
+#             print("Too low! Try again!")
+#         elif guess > answer:
+#             print("Too high!Try again")
+#         else:
+#             print(f"CORRECT! The answer was {answer}")
+#             print(f"Number of guesses: {guesses}")
+#             is_running = False
 
-    else:
-        print("Invalid guess")
-        print(f"Please select a number between {lowest_num} and {highest_num}")
+#     else:
+#         print("Invalid guess")
+#         print(f"Please select a number between {lowest_num} and {highest_num}")
 
 
 # print(answer)
+
+
+options = ("rock", "paper", "scissors")
+running= True
+
+while running:
+
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice(rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
+    else:
+        print("You lose!")
+    if not input("Play again? (y/n): ").lower() == "y":
+        running = False
+
+print("Thanks for playing!")
+
+
