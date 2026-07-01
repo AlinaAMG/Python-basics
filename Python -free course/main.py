1401fd7007c4c1a37aa250024454442fc1ec208c
Alina <alina_gabur@yahoo.com>
2026-07-01 17:57:30 +0200

    arguments in python

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -972,3 +972,92 @@ def count(end,start=0):
 
 print(count(0,10))
 
+# args              = allows you to pass multiple non-key arguments
+# ** kwargs         = allows you to pass multiple keywords-arguments
+# keyword arguments = an argument preceded by an identifier
+#                     allows you to pass multiple keyword-arguments
+#                    helps with readability
+#                    order of arguments doesn't matter
+#                    1. positional 2. default 3.KEYWORD 4.ARBITRARY
+
+# def hello(greeting, title, first, last):
+#     print(f"{greeting} {title} {first} {last}")
+
+# # hello("Hello", title = "Mr.", first = "Spongebob", last = "Squarepants")
+
+# hello("Hello", title = "Mr.", last = "John", first ="James")
+
+# for x in range(1, 11):
+#     print(x,end=" ")
+
+# print("1", "2", "3", "4", "5", sep="-")
+
+def get_phone(country, area,first, last):
+    return f"{country}-{area}-{first}-{last}"
+
+phone_num = get_phone(country=1, area=123, first=456, last=7890)
+
+print(phone_num)
+
+# Arbitrary arguments
+
+# def add(a,b):
+#     return a + b
+
+# print(add(1, 2, 3))
+
+# def add(*args): # je kan ook andere parameters gebruiken in plaats van args   
+#     total = 0
+#     for arg in args:
+#         total += arg
+#     return total
+
+# print(add(1, 2, 3,4,5))
+
+def display_name(*args):
+    for arg in args:
+        print(arg, end=" ")
+
+display_name("Dr.", "Spongebob", "Harold", "Squarepants", "III")
+
+# def print_address(**kwargs):
+#     for value in kwargs.values():
+#         print(value)
+
+# def print_address(**kwargs):
+#     for key in kwargs.keys():
+#         print(key)
+
+def print_address(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+
+print_address(street="123 Fake St.", 
+              city="Detroit", 
+              apt="100",
+              state="MI", 
+              zip="54321",)
+
+def shipping_label(*args, **kwargs):
+    for arg in args:
+        print(arg, end=" ")
+    print()
+
+    if "apt" in kwargs:    
+        print(f"{kwargs.get("street")} {kwargs.get("apt")}")
+    elif "pobox" in kwargs:
+        print(f"{kwargs.get("street")}")
+        print(f"{kwargs.get("pobox")}")
+    else:
+        print(f"{kwargs.get("street")}")
+
+    print(f"{kwargs.get("city")} {kwargs.get("state")}, {kwargs.get("zip")}, {kwargs.get("pobox")}")
+
+shipping_label("Dr.", "Spongebob", "Squarepants", "III",
+               street = "123 Fake St.",
+               apt="#100",
+               pobox ="PO box #1001",
+               city= "Detroit",
+               state="MI",
+               zip= "54321")
