e3974870e0fac01a8874618258957ab461263ffb
Alina <alina_gabur@yahoo.com>
2026-07-02 18:47:38 +0200

    membership operators

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -1096,3 +1096,49 @@ my_dictionary = {"A": 1, "B": 2, "C": 3}
 
 for key, value in  my_dictionary.items():
     print(f"{key}: {value}")
+
+
+# Membership operators = used to test wether a value or variable is found in  a   #                        sequence(string, list, tuple, set or dictionary)
+#                       1. in
+#                       2. not in
+
+word = "APPLE"
+letter = input("Guess a letter in the secret word: ")
+
+if letter not in word:
+    print(f"There is a {letter}")
+else:
+    print(f"{letter} was not found")
+
+students = {"Spongebob", "Patrick", "Sandy"}
+student = input("Enter the name of a student: ")
+
+# if student in students:
+#     print(f"{student} is a student")
+# else:
+#     print(f"{student} was not found")
+
+# if student not in students:
+#     print(f"{student} was not found")
+# else:
+#     print(f"{student} is a student")
+
+grades = {
+    "Sandy": "A",
+    "Squidward": "B",
+    "Spongebob":"C",
+    "Patrick":"D"
+}
+student = input("Enter the name of a student: ")
+
+if student in grades:
+    print(f"{student}'s grade is {grades[student]}")
+else:
+    print(f"{student} was not found")
+
+email = "BroCode@gmail.com"
+
+if "@" in email and "." in email:
+    print("Valid email")
+else:
+   print("Invalid email")
