8b39215e59222f2035b0fecc164f78555cad8d5c
Alina <alina_gabur@yahoo.com>
2026-07-02 17:20:47 +0200

    iterables

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -1061,3 +1061,38 @@ shipping_label("Dr.", "Spongebob", "Squarepants", "III",
                city= "Detroit",
                state="MI",
                zip= "54321")
+
+
+# Iterables = An object/collection that can return its elements one at a time,
+#             allowing it to be iterated over in a loop
+
+numbers = [1,2,3,4,5]
+
+# for number in  numbers:
+#     print(number)
+
+# for number in reversed(numbers):
+#     print(number, end = " - ")
+
+fruits = {"apple", "orange", "banana", "coconut"}
+
+# for fruit in fruits:
+#     print(fruit)
+
+# name = "Bro Code"
+
+# for character in name:
+#     print(character, end = " ")
+
+# Dictionary
+
+my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+# for key in my_dictionary:
+#     print(key)
+
+# for value in my_dictionary.values():
+#     print(value)
+
+for key, value in  my_dictionary.items():
+    print(f"{key}: {value}")
