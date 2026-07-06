73e107101efe7056b0557e9f4f66472b4a97c305
Alina <alina_gabur@yahoo.com>
2026-07-06 16:15:07 +0200

    match-case statement

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -1179,7 +1179,44 @@ print(fruit_chars)
 # print(positive_nums)
 # print(negative_nums)
 
-grades = [85, 42, 79, 90, 56, 61, 30]
-passing_grades = [grade for grade in grades if grade >= 60]
+# grades = [85, 42, 79, 90, 56, 61, 30]
+# passing_grades = [grade for grade in grades if grade >= 60]
 
-print(passing_grades)
+# print(passing_grades)
+
+
+# Match-case statement (switch): An alternative to using many "elif" statements
+#                               Execute some code if a values matches a "case"
+#                               Benefits: cleaner and syntax is more readable
+
+# def day_of_week(day):
+#     match day:
+#         case 1:
+#             return "It is Sunday"
+#         case 2:
+#             return "It is Monday"
+#         case 3:
+#             return "It is Tuesday"
+#         case 4:
+#             return "It is Wednesday"
+#         case 5:
+#             return "It is Thursday"
+#         case 6:
+#             return "It is Friday"
+#         case 7:
+#             return "It is Saturday"
+#         case _:
+#             return "Not a valid day"
+
+# print(day_of_week(4))
+
+def is_weekend(day):
+    match day:
+        case "Saturday" | "Sunday":
+            return True
+        case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+            return False
+        case _:
+            return False
+
+print(is_weekend("Monday"))
