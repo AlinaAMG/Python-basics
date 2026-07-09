0b56e30ad33780b1c4637da7177b87f8aef11420
Alina <alina_gabur@yahoo.com>
2026-07-09 18:14:07 +0200

    hangman in Python

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -1439,27 +1439,133 @@ if __name__ == "__main__":
 #     main()
 
 
-import string
+# import string
 
-chars = string.whitespace + string.punctuation + string.digits + string.ascii_letters
-chars = list(chars)
-key = chars.copy()
-random.shuffle(key)
+# chars = string.whitespace + string.punctuation + string.digits + string.ascii_letters
+# chars = list(chars)
+# key = chars.copy()
+# random.shuffle(key)
 
-print(f"chars: {chars}")
-print(f"key: {key}")
+# print(f"chars: {chars}")
+# print(f"key: {key}")
 
 
-print(chars)
+# print(chars)
 
-# ENCRYPT
+# # ENCRYPT
 
-plain_text = input("Enter a message to encrypt: ")
-cipher_text = ""
+# plain_text = input("Enter a message to encrypt: ")
+# cipher_text = ""
 
-for letter in plain_text:
-    index = chars.index(letter)
-    cipher_text += key[index]
+# for letter in plain_text:
+#     index = chars.index(letter)
+#     cipher_text += key[index]
 
-print(f"original message: {plain_text}")
-print(f"encrypted message: {cipher_text}")
+# print(f"original message: {plain_text}")
+# print(f"encrypted message: {cipher_text}")
+
+
+# Hangman in Python
+
+# dictionary of key:()
+words = ("apple", "orange", "banana", "coconut", "pineapple")
+
+hangman_art = {0: ("     ",
+                   "     ",
+                   "     "),
+               1: ("  o  ",
+                   "     ",
+                   "     "),
+               2: ("  o   ",
+                   "  |   ",
+                   "     "),
+               3: ("  o   ",
+                   " /|   ",
+                   "     "),
+               4: ("  o   ",
+                   " /|\\ ",
+                   "     "),
+               5: ("  o   ",
+                   " /|\\  ",
+                   " /   "),
+               6 :("  o   ",
+                   " /|\\  ",
+                   " / \\  "),
+                }
+
+def display_man(wrong_guesses):
+    print("**********************")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("**********************")
+
+def display_hint(hint):
+    print(" ".join(hint))
+
+def display_answer(answer):
+    print(" ".join(answer))
+
+def main():
+    answer = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses  = 0
+    guesses_letters = set()
+    is_running = True
+
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input")
+            continue
+
+        if guess in guesses_letters:
+            print(f"{guess} is already guessed ")
+            continue
+
+        guesses_letters.add(guess)
+
+
+
+        if guess in answer:
+           for i in range(len(answer)):
+              if answer[i] == guess:
+                hint[i] = guess
+        else:
+            wrong_guesses += 1
+
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU WIN!!")
+            is_running = False
+        elif wrong_guesses >= len(hangman_art) - 1 :
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU LOSE!")
+            is_running= False
+
+
+
+if __name__ == "__main__":
+    main()
+
+    #PYthon Object Oriented Programming
+
+# object = A "Bundle" of related attributes (variables) and methods (functions)
+#         Ex. phone, cup, book
+#         You need a  "class"  to create many objects
+
+# class = (blueprint) used to design the structure and layout of an object
+
+class Car:
+    def __init__(self, model, year, color, for_sale):
+        self.model = model
+        self.year = year
+        self.color = color
+        self.for_sale = for_sale
+
+car1 = Car("Mustang", 2025, "red", False)
+print(car1)
