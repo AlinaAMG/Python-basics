f9052a21d9d18b1315cb725287292c95b614d0b2
Alina <alina_gabur@yahoo.com>
2026-07-10 13:38:25 +0200

    class variables

--- Python -free course/main.py
+++ Python -free course/main.py
@@ -1592,5 +1592,34 @@ print(car3.color)
 print(car3.for_sale)
 
 
+# class variables = Shared among all instances of a class
+#                  Defined outside the constructor
+#                 Allow you to share data among all objects created from that class
+
+class Student:
+
+    class_year = 2026
+    num_students = 0
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age 
+        Student.num_students += 1
+
+student1 = Student("Spongebob", 30)
+student2  = Student("Patrick", 35)
+student3 = Student("Aryan", 10)
+student4 = Student("Mike", 43)
+
+# print(student1.name)
+# print(student1.age)
+# print(student2.class_year)
+# print(Student.num_students)
+print(f"My graduating class of {Student.class_year} has {Student.num_students} students")
+print(student1.name)
+print(student2.name)
+print(student3.name)
+print(student4.name)
+
 
 
